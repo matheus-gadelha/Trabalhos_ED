f498651536fd2cb90a2574cc823f1d84143c5c19
matheus-gadelha <matheusgadelha2000@gmail.com>
2019-03-25 09:25:14 -0300

    arvore

--- 04_fractais/arvore/arvore.c
+++ 04_fractais/arvore/arvore.c
@@ -2,25 +2,62 @@
 #include <stdlib.h>
 
 void fractal(float x, float y, float ang, float lado){
-    float delta = 35 + xm_rand(-2, 2); /* angulo de rotacao */
-    float dec = 0.72 + xm_rand(-20, 20)/100.0;
-    if(lado < 10)
+    int fruta = xm_rand(0, 100);
+
+    if(lado < xm_rand(8, 20)){
+        if(fruta <= 20){
+            xs_color((XColor){xm_rand(0, 255), xm_rand(0, 255), xm_rand(0, 255)});
+            xd_filled_circle(x + lado * xm_cos(ang), y - lado * xm_sin(ang), 8);
+        }
+        
         return;
+    }
     
-    float fx = x + lado * xm_cos(ang);
-    float fy = y - lado * xm_sin(ang);
-    xd_line(x, y, fx, fy); /* desenha a linha */
+    float fx = x + lado * xm_cos(ang); //Descobre a coordenada do x final
+    float fy = y - lado * xm_sin(ang); //Descobre a coordenada do y final
     
-    fractal(fx, fy, ang - delta, lado * dec);
-    fractal(fx, fy, ang        , lado * dec);
-    fractal(fx, fy, ang + delta, lado * dec);
+    if(lado > 70.0){
+        int galhos = xm_rand(0, 100);
+        xs_color((XColor){139, 69, 19});
+        xd_thick_line(x, y, fx, fy, 7);
+
+        if(galhos <= 40){
+            xs_color((XColor){139, 69, 19});
+            fractal(fx, fy, ang, lado - xm_rand(8, 15) );
+        }
+
+
+    }else if(lado > 50.0){
+        xs_color((XColor){160, 82, 45});
+        xd_thick_line(x, y, fx, fy, 5);
+        
+
+    }else if(lado > 30.0){
+        int galhos = xm_rand(0, 100);
+        xs_color((XColor){205, 133, 63});
+        xd_thick_line(x, y, fx, fy, 2);
+
+        if(galhos <= 60){
+            xs_color((XColor){205, 133, 63});
+            fractal(fx, fy, ang, lado - xm_rand(8, 15) );
+        }
+        
+        
+    }
+
+    xd_thick_line(x, y, fx, fy, 2); /* desenha a linha */
+    x_step("arvore");
+    
+    fractal(fx, fy, ang - xm_rand(40, 55) , lado - xm_rand(8, 15) );
+    
+    fractal(fx, fy, ang + xm_rand(40, 55), lado - xm_rand(8, 15) );
 }
 
 int main(){
-    x_open(800, 500);
+    x_open(800, 600);
     srand(0); /* comente para ter sempre uma arvore diferente */
-    fractal(400, 450, 90, 100);
-    x_save("figura_arvore");
+    fractal(400, 550, 90, 100);
+    x_save("arvore");
     x_close();
     return 0;
 }
